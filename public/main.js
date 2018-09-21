c056bf59b56878597b94522473a72d0ab92bef2b
briankoech <brnkirwa@gmail.com>
2018-09-21 04:34:56 +0300

    Add messages display on dashboard

--- public/main.js
+++ public/main.js
@@ -7,9 +7,10 @@ var config = {
   storageBucket: '',
   messagingSenderId: '52464281842'
 };
+
 firebase.initializeApp(config)
 
-function loginUser() {
+const loginUser = function() {
   showLoading();
   var provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function(resutl) {
@@ -38,12 +39,11 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 
 // fetch current user details
-var getCurrentUser = function() {
-
-};
 
 // set user profile details
-var setCurrentUserProfile = function(user) {
+let receipient, currentUserId;
+const setCurrentUserProfile = function(user) {
+  currentUserId = user.uid;
   const profile = document.getElementById('profile');
   const img = profile.querySelector('img');
   img.src = user.photoURL;
@@ -60,12 +60,26 @@ var setCurrentUserProfile = function(user) {
 };
 
 // get all users on the app
-var getAllUsers = function () {
+let firstLoaded = false;
+const getAllUsers = function () {
   firebase.database().ref(`users/`).on('value', function(snapshot) {
     const users = snapshot.val();
+    // clear the container
+    const chats = document.getElementById('chats');
+    const chatsList = chats.querySelector('ul');
+    while(chatsList.firstChild) {
+      chatsList.removeChild(chatsList.firstChild);
+    }
+
     // loop through all the users appending list on the sidebar
-    Object.keys(users).forEach(userId => {
+    Object.keys(users).forEach((userId) => {
       const user = users[userId];
+      if (userId == currentUserId) return;
+
+      if (!firstLoaded) {
+        showChats(userId);
+        firstLoaded = true;
+      }
       createList(user);
     });
   });
@@ -87,9 +101,66 @@ const createList = function(user) {
 };
 
 const showChats = function(userId) {
-  console.log(userId);
-    // show chats
+  getUser(userId);
+  getMessages(userId);
+};
+
+const getMessages = function(userId) {
+  const messagesContainer = document.getElementById('messages');
+  const list = messagesContainer.querySelector('.messages-list');
+  firebase.database().ref(`/chats/${currentUserId}/${userId}`).on('value', function(snapshot) {
+    const messages = snapshot.val();
+    // remove everything from the receiver
+    while(list.firstChild) {
+      list.removeChild(list.firstChild)
+    }
+
+    if (!messages) return;
+
+    Object.keys(messages).forEach(key => {
+      // append message to messages-list
+      const message = messages[key];
+      if (message.type == 'text') {
+        const div = document.createElement('div');
+        div.className = 'message';
+        div.innerHTML = `${message.message}`;
+        list.appendChild(div);
+        if (message.sender == currentUserId) div.classList.add('from-me');
+      }
+    });
+  });
 }
+
+const getUser = function(userId) {
+  firebase.database().ref(`/users/${userId}`).once('value').then(function(snapshot) {
+    const user = snapshot.val();
+    if (!user) return;
+    receipient = userId;
+    // update username and profile picture
+    const html = `
+      <img src="${user.photoURL}" alt="Active user">
+      <span>${user.displayName || 'Anonymous'}</span>
+    `;
+    const receiver = document.getElementById('receiver');
+    receiver.innerHTML = html;
+  });
+}
+
+const sendMessage = function() {
+  // grab message from text field
+  const textarea = document.getElementById('textarea');
+  if (!textarea.value) return;
+  const messageRef = firebase.database().ref(`/chats/${currentUserId}/${receipient}`).push({
+    type: 'text', message: textarea.value, sender: currentUserId
+  });
+  // push the same message to recipients chats
+  firebase.database().ref(`/chats/${receipient}/${currentUserId}/${messageRef.key}`).set({
+    type: 'text', message: textarea.value, sender: currentUserId
+  });
+
+  textarea.value = '';
+}
+
 function logout() {
   showLoading();
   firebase.auth().signOut()
